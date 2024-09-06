2507261ce62762d246adea88a12ea65a3fb6fcc0
TimRa804 <timra804@gmail.com>
2024-09-06 10:58:23 -0500

    edited tf files

--- main.tf
+++ main.tf
@@ -1,5 +1,13 @@
 resource "kubernetes_namespace" "example" {
   metadata {
-    name = "my-first-namespace"
+    name = var.name
   }
 }
+
+
+variable name {
+  type = string
+  default = "test"
+  description = "The name of the namespace"
+}
+
---
+++ variable.tf
@@ -0,0 +1,6 @@
+variable name {
+  type = string
+  default = "test"
+  description = "The name of the namespace"
+}
+
